49e72c687025d847f5a983005e70855142853af9
Yuval Adam <yuval@y3xz.com>
2016-08-15 16:54:55 +0300

    Add QR size param

--- index.js
+++ index.js
@@ -2,36 +2,41 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import QRious from 'qrious'
 
+import './base.scss'
+
 class Qrgen extends React.Component {
 
   constructor (props) {
     super(props)
     this.state = {
-      'text': 'abcde'
+      text: 'Hello World!',
+      size: 100
     }
   }
 
-  updateQr () {
-    const qr = new QRious({
-      element: document.getElementById('qr'),
-      value: this.state.text
+  componentDidMount () {
+    this.qr = new QRious({
+      element: this.refs.qr,
+      value: this.state.text,
+      size: this.state.size
     })
   }
 
   componentDidUpdate (prevProps, prevState) {
-    if (this.state.text != prevState.text) {
-      this.updateQr()
-    }
+    this.qr.value = this.state.text
+    this.qr.size = this.state.size
   }
 
   render () {
     return <div id='root'>
       <h1>qrgen</h1>
-      <canvas id='qr'></canvas>
+      <canvas ref='qr' id='qr'></canvas>
       <input type='text' value={this.state.text} onChange={(e) => {
-        this.setState({ text: e.target.value })
+        this.setState(Object.assign({}, this.state, { text: e.target.value }))
       }}></input>
-      <p>{this.state.text}</p>
+      <input type='text' value={this.state.size} onChange={(e) => {
+        this.setState(Object.assign({}, this.state, { size: e.target.value }))
+      }}></input>
     </div>
   }
 }
