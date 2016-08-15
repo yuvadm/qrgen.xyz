36dca367fd4e0d531b025f5ca0e5eb9396d89a30
Yuval Adam <yuval@y3xz.com>
2016-08-15 16:55:04 +0300

    Add SCSS style

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,15 @@
+var bourbon = require('node-bourbon').includePaths
+
 module.exports = {
-    entry: './index.js',
-    output: {
-        path: __dirname,
-        filename: 'bundle.js'
-    },
-    module: {
-        loaders: [
-            { test: /\.js$/, loader: 'babel', },
-            { test: /\.css$/, loader: 'style!css' }
-        ]
-    }
+  entry: './index.js',
+  output: {
+    path: __dirname,
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      { test: /\.js$/, loader: 'babel', },
+      { test: /\.scss$/, loader: 'style!css!sass?includePaths[]=' + bourbon }
+    ]
+  }
 };
